aedcb02f19eb686614ebe342aa7d47920918769f
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-05-13 15:11:01 -0700

    fix(Match)!: remove unnecessary dependencies
    Assign the method a few but clearly defined tasks to make testing
    the interface easier.
    
    * fix(Match)!: remove individual public player properties
        - unnecessary binding to a implementation
    
    * fix(Match tests): remove implementation specific tests and just check
    for the assigned task
    
    BREAKING CHANGE:
        - player1 and player2 properties removed, check players
        with activePlayer property

--- src/logic/Match.js
+++ src/logic/Match.js
@@ -1,10 +1,7 @@
-import { Player } from "./index.js";
-
 class Match {
   #mode;
   #activePlayer;
-  #player1;
-  #player2;
+  #players = new Map();
 
   get mode() {
     return this.#mode;
@@ -14,84 +11,41 @@ class Match {
     return this.#activePlayer;
   }
 
-  get player1() {
-    return this.#player1;
-  }
-
-  get player2() {
-    return this.#player2;
-  }
-
   #validateMode(mode) {
-    if (typeof mode !== "string")
-      throw new TypeError(`${typeof mode} <- Mode must be a string.`);
-
     const isValidMode = mode === "single" || mode === "double";
     if (!isValidMode)
       throw new ReferenceError(`${mode} <- Mode is an invalid mode.`);
-  }
 
-  #validatePlayerNames(...names) {
-    names.forEach((name, index) => {
-      if (typeof name !== "string")
-        throw new TypeError(
-          `(${typeof name}) <- Player${index + 1} name must be a string.`
-        );
-    });
-  }
-
-  #validatePlayerObjects() {
-    if (this.#player1 === undefined || this.#player2 === undefined)
-      throw new ReferenceError(
-        "Players not found. Call the setPlayers() method first."
-      );
+    return mode;
   }
 
   setMode(modeName) {
-    this.#validateMode(modeName);
-
-    this.#mode = modeName;
+    this.#mode = this.#validateMode(modeName);
 
     return this;
   }
 
-  setPlayers(player1Name, player2Name) {
-    if (this.#mode === undefined)
-      throw new ReferenceError(
-        "Mode not found. Set mode first by calling setMode() method."
-      );
-
-    if (this.#mode === "single") {
-      this.#validatePlayerNames(player1Name);
-
-      this.#player1 = new Player("real").setName(player1Name);
-      this.#player2 = new Player().setName("Opponent");
-    } else if (this.#mode === "double") {
-      this.#validatePlayerNames(player1Name, player2Name);
-
-      this.#player1 = new Player("real").setName(player1Name);
-      this.#player2 = new Player("real").setName(player2Name);
-    }
+  setPlayers(player1, player2) {
+    this.#players.set("player1", player1);
+    this.#players.set("player2", player2);
 
     return this;
   }
 
   switchTurn() {
-    this.#validatePlayerObjects();
-
     const NO_ACTIVE_PLAYER = undefined;
 
     switch (this.#activePlayer) {
       case NO_ACTIVE_PLAYER:
-        this.#activePlayer = this.#player1;
+        this.#activePlayer = this.#players.get("player1");
         break;
 
-      case this.#player2:
-        this.#activePlayer = this.#player1;
+      case this.#players.get("player2"):
+        this.#activePlayer = this.#players.get("player1");
         break;
 
-      case this.#player1:
-        this.#activePlayer = this.#player2;
+      case this.#players.get("player1"):
+        this.#activePlayer = this.#players.get("player2");
         break;
 
       default:
--- src/logic/tests/Match.test.js
+++ src/logic/tests/Match.test.js
@@ -1,4 +1,4 @@
-import { Match, Player } from "../index.js";
+import { Match } from "../index.js";
 import { isInheritedProperty, hasMethod } from "./utils/index.js";
 
 describe("Match constructor", () => {
@@ -39,32 +39,6 @@ describe("activePlayer property", () => {
   });
 });
 
-describe("player1 property", () => {
-  let match;
-  beforeEach(() => {
-    match = new Match();
-  });
-
-  it("exists", () => isInheritedProperty(match, "player1"));
-
-  it("starts as undefined", () => {
-    expect(match.player1).toBeUndefined();
-  });
-});
-
-describe("player2 property", () => {
-  let match;
-  beforeEach(() => {
-    match = new Match();
-  });
-
-  it("exists", () => isInheritedProperty(match, "player2"));
-
-  it("starts as undefined", () => {
-    expect(match.player2).toBeUndefined();
-  });
-});
-
 describe("setMode method", () => {
   it("exists", () => hasMethod(Match, "setMode"));
 
@@ -82,107 +56,18 @@ describe("setMode method", () => {
     expect(match.mode).toBe("double");
   });
 
-  it("throws TypeError for non-string mode", () => {
-    expect(() => match.setMode()).toThrow(TypeError);
-    expect(() => match.setMode(null)).toThrow(TypeError);
-    expect(() => match.setMode(true)).toThrow(TypeError);
-    expect(() => match.setMode(1)).toThrow(TypeError);
-    expect(() => match.setMode(1n)).toThrow(TypeError);
-    expect(() => match.setMode({})).toThrow(TypeError);
-    expect(() => match.setMode([])).toThrow(TypeError);
-    expect(() => match.setMode(() => {})).toThrow(TypeError);
-  });
-
   it("throws ReferenceError for invalid modes", () => {
     expect(() => match.setMode("triple")).toThrow(ReferenceError);
-  });
-});
 
-describe("setPlayers method", () => {
-  it("exists", () => hasMethod(Match, "setPlayers"));
-
-  let match;
-  beforeEach(() => {
-    match = new Match();
-  });
-
-  it("throws ReferenceError when mode is undefined", () => {
-    expect(match.mode).toBeUndefined();
-
-    expect(() => match.setPlayers("", "")).toThrow(ReferenceError);
-  });
-
-  it("throws TypeError when passing in non strings names", () => {
-    match.setMode("single");
-    expect(() => match.setPlayers()).toThrow(TypeError);
-    expect(() => match.setPlayers(null)).toThrow(TypeError);
-    expect(() => match.setPlayers(true)).toThrow(TypeError);
-    expect(() => match.setPlayers(1)).toThrow(TypeError);
-    expect(() => match.setPlayers(1n)).toThrow(TypeError);
-    expect(() => match.setPlayers({})).toThrow(TypeError);
-    expect(() => match.setPlayers([])).toThrow(TypeError);
-    expect(() => match.setPlayers(() => {})).toThrow(TypeError);
-
-    match.setMode("double");
-    expect(() => match.setPlayers("")).toThrow(TypeError);
-    expect(() => match.setPlayers("", null)).toThrow(TypeError);
-    expect(() => match.setPlayers("", true)).toThrow(TypeError);
-    expect(() => match.setPlayers("", 1)).toThrow(TypeError);
-    expect(() => match.setPlayers("", 1n)).toThrow(TypeError);
-    expect(() => match.setPlayers("", {})).toThrow(TypeError);
-    expect(() => match.setPlayers("", [])).toThrow(TypeError);
-    expect(() => match.setPlayers("", () => {})).toThrow(TypeError);
-  });
-
-  it("creates players from Player constructor", () => {
-    match.setMode("single").setPlayers("", "");
-
-    expect(match.player1).toBeInstanceOf(Player);
-    expect(match.player2).toBeInstanceOf(Player);
-  });
-
-  it("sets single player type to real and other to computer", () => {
-    match.setMode("single").setPlayers("bob");
-
-    expect(match.player1.type).toBe("real");
-    expect(match.player2.type).toBe("computer");
-  });
-
-  it("sets both players' type to real", () => {
-    match.setMode("double").setPlayers("bill", "joe");
-
-    expect(match.player1.type).toBe("real");
-    expect(match.player2.type).toBe("real");
-  });
-
-  it("sets players with correct names for single", () => {
-    match.setMode("single").setPlayers("zoe");
-
-    expect(match.player1.name).toBe("zoe");
-    expect(match.player2.name).toBe("Opponent");
-  });
-
-  it("sets players with correct names for double", () => {
-    match.setMode("double").setPlayers("joe", "jane");
-
-    expect(match.player1.name).toBe("joe");
-    expect(match.player2.name).toBe("jane");
-  });
-
-  it("does not set players to same object reference for single", () => {
-    match.setMode("single").setPlayers("", "");
-
-    expect(match.player1).not.toBe(match.player2);
-  });
-
-  it("does not set players to same object reference for double", () => {
-    match.setMode("double").setPlayers("", "");
-
-    expect(match.player1).not.toBe(match.player2);
-  });
-
-  it("returns match instance", () => {
-    expect(match.setMode("single").setPlayers("", "")).toBe(match);
+    expect(() => match.setMode()).toThrow(ReferenceError);
+    expect(() => match.setMode("")).toThrow(ReferenceError);
+    expect(() => match.setMode(null)).toThrow(ReferenceError);
+    expect(() => match.setMode(true)).toThrow(ReferenceError);
+    expect(() => match.setMode(1)).toThrow(ReferenceError);
+    expect(() => match.setMode(1n)).toThrow(ReferenceError);
+    expect(() => match.setMode({})).toThrow(ReferenceError);
+    expect(() => match.setMode([])).toThrow(ReferenceError);
+    expect(() => match.setMode(() => {})).toThrow(ReferenceError);
   });
 });
 
@@ -194,61 +79,101 @@ describe("switchTurn method", () => {
     match = new Match();
   });
 
-  it("throws ReferenceError when mode is not set", () => {
-    expect(match.mode).toBeUndefined();
-
-    expect(() => match.switchTurn()).toThrow(ReferenceError);
-  });
-
-  it("throws ReferenceError when players are not set", () => {
-    expect(match.player1).toBeUndefined();
-    expect(match.player2).toBeUndefined();
-
-    expect(() => match.switchTurn()).toThrow(ReferenceError);
-  });
-
-  it("chooses activePlayer as player1 when activePlayer is undefined", () => {
+  it("sets activePlayer as 1st player when activePlayer is undefined", () => {
     expect(match.activePlayer).toBeUndefined();
 
-    match.setMode("single").setPlayers("").switchTurn();
-    expect(match.activePlayer).toBe(match.player1);
+    match.setPlayers("John", "Hill");
+    match.switchTurn();
+    expect(match.activePlayer).toBe("John");
   });
 
-  it("switches turn to player2", () => {
-    match.setMode("single").setPlayers("");
+  it("switches turn to 2nd player", () => {
+    match.setPlayers("John", "Hill");
 
     match.switchTurn();
-    expect(match.activePlayer).toBe(match.player1);
-
     match.switchTurn();
-    expect(match.activePlayer).toBe(match.player2);
+    expect(match.activePlayer).toBe("Hill");
   });
 
-  it("switches turn back to player1", () => {
-    match.setMode("double").setPlayers("", "");
+  it("switches turn back 1st Player", () => {
+    match.setPlayers("John", "Hill");
 
     match.switchTurn();
-    expect(match.activePlayer).toBe(match.player1);
-
     match.switchTurn();
-    expect(match.activePlayer).toBe(match.player2);
-
     match.switchTurn();
-    expect(match.activePlayer).toBe(match.player1);
+    expect(match.activePlayer).toBe("John");
   });
 
   it("switches turn multiple times", () => {
-    match.setMode("double").setPlayers("", "");
+    const players = ["John", "Hill"];
 
+    match.setPlayers(players[0], players[1]);
     for (let i = 0; i < 100; ++i) {
       match.switchTurn();
-      expect(match.activePlayer).toBe(match[`player${(i % 2) + 1}`]);
+      expect(match.activePlayer).toBe(players[i % players.length]);
     }
+  });
+
+  it("switches turn with player objects by reference", () => {
+    const player1 = {};
+    const player2 = {};
+    match.setPlayers(player1, player2);
+
+    match.switchTurn();
+    expect(match.activePlayer).toBe(player1);
+    expect(match.activePlayer).not.toBe(player2);
+
+    match.switchTurn();
+    expect(match.activePlayer).toBe(player2);
+    expect(match.activePlayer).not.toBe(player1);
   });
 
   it("returns match instance", () => {
     match.setMode("double").setPlayers("", "");
 
     expect(match.switchTurn()).toBe(match);
+  });
+});
+
+describe("setPlayers method", () => {
+  it("exists", () => hasMethod(Match, "setPlayers"));
+
+  let match;
+  beforeEach(() => {
+    match = new Match();
+  });
+
+  it("sets players", () => {
+    match.setPlayers("john", "jill");
+
+    match.switchTurn();
+    expect(match.activePlayer).toBe("john");
+    match.switchTurn();
+    expect(match.activePlayer).toBe("jill");
+  });
+
+  it("sets players by object reference", () => {
+    const john1 = { name: "john" };
+    const john2 = { name: "john" };
+    match.setPlayers(john1, john2);
+
+    match.switchTurn();
+    expect(match.activePlayer).toBe(john1);
+    expect(match.activePlayer).not.toBe(john2);
+
+    match.switchTurn();
+    expect(match.activePlayer).toBe(john2);
+    expect(match.activePlayer).not.toBe(john1);
+  });
+
+  it("sets with only one player", () => {
+    match.setPlayers("John");
+
+    match.switchTurn();
+    expect(match.activePlayer).toBe("John");
+  });
+
+  it("returns match instance", () => {
+    expect(match.setMode("single").setPlayers("", "")).toBe(match);
   });
 });
